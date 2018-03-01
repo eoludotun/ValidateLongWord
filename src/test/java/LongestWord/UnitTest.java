01b650f8cd197a0ebe6d7134450f947b08c259f4
eoludotun <eoludotun@users.noreply.github.com>
2018-02-28 19:32:42 -0500

    Update UnitTest.java

--- src/test/java/LongestWord/UnitTest.java
+++ src/test/java/LongestWord/UnitTest.java
@@ -17,14 +17,16 @@ public class UnitTest {
 	@Test
 	public void testWordCount(String countWords) {
         longestWordFinder.validateMaxWord(countWords);
-		System.out.println("Expected long word is : jumped " + " Actual: " + longestWordFinder.getmaxWord());
-		Assert.assertEquals(longestWordFinder.getmaxWord(), "jumped");
+	System.out.println("Expected long word is : jumped " + " Actual: " + longestWordFinder.getmaxWord());
+	Assert.assertEquals(longestWordFinder.getmaxWord(), "jumped");
 	}
 			
 	@Test
 	public void testlengthOfCharacter(String wordlength) {
-		longestWordFinder.validateMaxWord(wordlength);
-		System.out.println("Expected word length is : 6 " + " Actual: " + longestWordFinder.getmaxlength());
-		Assert.assertEquals(longestWordFinder.getmaxlength(), 6);
-		}
+	longestWordFinder.validateMaxWord(wordlength);
+	System.out.println("Expected word length is : 6 " + " Actual: " + longestWordFinder.getmaxlength());
+	Assert.assertEquals(longestWordFinder.getmaxlength(), 6);
+	}
 }
+		
+
